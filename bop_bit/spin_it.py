1824c94911def5c309a70b3abc0bad0b19e106f8
lily283 <leyang2010@gmail.com>
2018-01-11 17:48:39 +1100

    Started spin module

--- bop_bit/spin_it.py
+++ bop_bit/spin_it.py
@@ -1,2 +1,19 @@
 # Spin it
 # https://trello.com/c/uUocRhe4
+
+from microbit import *
+import radio
+
+radio.on()
+radio.config(channel=69)
+
+def is_triggered()
+    zeroval = pin0.read_digital()
+    pin0.set_pull(pin0.PULL_UP)
+    oneval = pin1.read_digital()
+    pin1.set_pull(pin0.PULL_UP)
+
+    if zeroval == 1 or oneval == 1:
+        return True
+    else:
+        return False

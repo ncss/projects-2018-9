4d2e335ac53a648a00edde61a456c62d413bb0f8
lily283 <leyang2010@gmail.com>
2018-01-12 14:49:06 +1100

    Added debug and client

--- bop_bit/shout_it.py
+++ bop_bit/shout_it.py
@@ -3,15 +3,20 @@
 
 from microbit import *
 import radio
+import client
 
 radio.on()
 radio.config(channel=69)
+client = Client(69, "shout_it")
+
+debug = False
 
 def is_triggered():
     prev_val = pin0.read_analog()
     sleep(200)
     pin_val = pin0.read_analog()
-    print(prev_val, pin_val)
+    if debug:
+        print(prev_value, cur_value)
     if abs(pin_val-prev_val) >= 250:
         return True
     else:
@@ -19,6 +24,6 @@ def is_triggered():
     
 while True:
     if is_triggered() == True:
-        print('yes')
-    else:
-        print('no')
+        client.send_trigger()
+        if debug:
+            print('yes')

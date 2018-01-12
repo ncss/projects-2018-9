febc97bfcb10a04d8ef692da7ff95c8fa9396277
AL34180 <AL34180@gmail.com>
2018-01-12 11:24:28 +1100

    returns state of the button

--- bop_bit/bop_it.py
+++ bop_bit/bop_it.py
@@ -1,2 +1,16 @@
 # Bop it
 # https://trello.com/c/vSm7z3AP
+
+from microbit import *
+#connect to pin 0 and 3V
+
+def is_triggered():
+    pressed = pin0.read_digital()
+    if pressed == 0:
+        print('yes')
+        return True
+    else:
+        print('no')
+        return False
+
+

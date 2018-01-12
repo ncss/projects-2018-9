6c3ef7a659774e8e7579af1bc05663e8caf5212b
IzzyN <isobel.norris@gmail.com>
2018-01-12 11:45:28 +1100

    Made colours dict, now listens to server

--- SimonSays/unit.py
+++ SimonSays/unit.py
@@ -1,35 +1,50 @@
 from microbit import *
 import radio
 import neopixel
-import random
+import random #currently not required, should only be used on central
 
 npix = neopixel.NeoPixel(pin0, 7)
 
 #colors
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-yellow = (255,255,0)
+colours = { 'red' : (255,0,0),
+            'green' : (0,255,0),
+            'blue' : (0,0,255),
+            'yellow' : (255,255,0) }
 
-def LightAll(col):
+
+def LightAll(colour):
     for pix in range(0, len(npix)):
-        npix[pix] = col
+        npix[pix] = colours[colour]
     npix.show()
     return
 
+set_up = False
+unit_name = ''
+
 #start up
 radio.on()
-radio.config(channel=73, group="ncssgroup2")
+radio.config(channel=73, group=2)
 #to get name from center unit
 radio.send("requestname")
-unit_name = radio.receive()
+
+while set_up == False:
+    msg = radio.receive()
+    if msg:
+        if msg.startswith('setup'):
+            unit_name = msg.split(':')[1]
+            display.scroll(unit_name)
+            print(unit_name)
+            set_up = True    
 
 while True:
     msg = radio.receive() #unit1:red EXAMPLE
-    if msg.startswith(unit_name):
-        colour = msg.split(':')[1]
-        LightAll(colour)
-        sleep(5000)
-        npix.clear()
-        break
-        
+    if msg:
+        print(msg)
+        if msg.startswith(unit_name):
+            print('in thing')
+            colour = msg.split(':')[1]
+            LightAll(colour)
+            sleep(5000)
+            npix.clear()
+            break
+    

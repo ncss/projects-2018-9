df600a9b44f251c79f926c41c519bab8e5897458
Liam Kinne <lkin8911@SHARED.SYDNEY.EDU.AU>
2018-01-12 15:10:21 +1100

    fixed syntax

--- bop_bit/bop_it.py
+++ bop_bit/bop_it.py
@@ -9,13 +9,13 @@ server = Server(69)
 debug = False
 
 def is_triggered():
-    pressed = pin0.read_digital()
-    if pressed == 0:
-        if debug print('yes')
-        return True
+	pressed = pin0.read_digital()
+	if pressed == 0:
+		if debug: print('yes')
+		return True
     else:
-        if debug print('no')
-        return False
+		if debug: print('no')
+		return False
 
 while True:
 	server.set_module_was_triggered("bop_it")

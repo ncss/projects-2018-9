87b0a075d9fdee0a16a57b10bf733a2f7136fe2d
Liam Kinne <lkin8911@SHARED.SYDNEY.EDU.AU>
2018-01-12 15:04:51 +1100

    put colons in for if statements

--- bop_bit/spin_it.py
+++ bop_bit/spin_it.py
@@ -14,7 +14,7 @@ prev_value = 0;
 
 def is_triggered():
     prev_value = pin1.read_analog()
-    if debug print(prev_value, cur_value)
+    if debug: print(prev_value, cur_value)
     if abs(cur_value-prev_value) >= 100:
         cur_value = prev_value
         return True
@@ -24,4 +24,4 @@ def is_triggered():
 while True:
     if is_triggered():
         client.send_trigger()
-        if debug print('yes')
+        if debug: print('yes')

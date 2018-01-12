362c16cde50983f41ce8cbc79d09f8e1b7e6cf39
lily283 <leyang2010@gmail.com>
2018-01-12 16:06:14 +1100

    Created pull_it

--- bop_bit/pull_it.py
+++ bop_bit/pull_it.py
@@ -1,25 +1,44 @@
-# Pull it
-# https://trello.com/c/3lagC1Oo
+from microbit import *
 import radio
 
 class Client:
-	"""Sends trigger messages to a server on the same channel"""
-	def __init__(self, chan, name):
-		self.channel = chan
-		self.name = name.lower()
-		radio.on()
-		radio.config(channel = self.channel)
-		#print("Configured Client")
+    """Sends trigger messages to a server on the same channel"""
+    def __init__(self, chan, name):
+        self.channel = chan
+        self.name = name.lower()
+        radio.on()
+        radio.config(channel = self.channel)
+        #print("Configured Client")
 
-	def send_trigger(self):
-		message = self.name
-		message += ":"
-		message += "triggered"
-		radio.send(message)
-		#print("Sent: " + message)
+    def send_trigger(self):
+        message = self.name
+        message += ":"
+        message += "triggered"
+        radio.send(message)
+        #print("Sent: " + message)
 
-	def send_alive(self):
-		message = self.name
-		message += ":"
-		message += "alive"
-		#print("Sent: " + message)
+    def send_alive(self):
+        message = self.name
+        message += ":"
+        message += "alive"
+        #print("Sent: " + message)
+
+client = Client(69, "pull_it")
+
+debug = False
+
+def is_triggered():
+    value = pin0.read_analog()
+    if debug:
+        print(value)
+    if value >= 1020:
+        return True
+    else:
+        return False
+
+while True:
+    if is_triggered():
+        client.send_trigger()
+        if debug:
+            print('yes')
+    sleep(200)

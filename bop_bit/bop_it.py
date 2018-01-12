3c04d4c95a7b3050ed5b4975189ad2972182e3d4
Liam Kinne <lkin8911@SHARED.SYDNEY.EDU.AU>
2018-01-12 15:40:31 +1100

    add server to main

--- bop_bit/bop_it.py
+++ bop_bit/bop_it.py
@@ -2,7 +2,33 @@
 # https://trello.com/c/vSm7z3AP
 
 from microbit import *
-import server
+import radio
+
+class Server:
+	"""docstring for Server"""
+	def __init__(self, chan):
+		self.channel = chan
+		self.modules = {}
+		radio.on()
+		radio.config(channel = self.channel)
+
+	def update(self):
+		message = radio.receive()
+		if message:
+			module, action = message.split(":")
+			self.modules[module] = True # add module
+
+	def get_module_was_triggered(self, module):
+		if module in self.modules:
+			state = self.modules[module]
+			self.modules[module] = False
+		else:
+			state = False
+		return state
+
+	def set_module_was_triggered(self, module)
+		if module in self.modules:
+			modules[module] = True
 
 server = Server(69)
 

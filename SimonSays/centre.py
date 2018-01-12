193a17638664eb17cca0f1e4a5e7e9caf121c0da
Will Bishop <will.bishop@gihs.sa.edu.au>
2018-01-12 11:06:20 +1100

    added start button

--- SimonSays/centre.py
+++ SimonSays/centre.py
@@ -1,17 +1,24 @@
-from microbit import *
+from quokka import *
 import radio
 
 radio.on()
+
+start_button = button_a #Change for whatever button it is configured to
 
 radio.config(channel=73, group="ncssgroup2") #Configures to radio to use its own channel to prevent noise
 
 units = [] #Units start as empty
 in_game = False
 def setup():
-    #give each unit name
+    unit_name = "unit%d" % len(units) #%d means that "%d" will be replaced with the length of units (the "d" is the type)
+    radio.send("setup:" + unit_name)
+    units.append(unit_name)
     
     
     
 while True:
     if radio.recieve() == 'requestname':
         setup()
+    if not in_game and start_button.read_digital():
+        in_game = True
+    display.show("Hey now")

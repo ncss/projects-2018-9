038910555147442f146dbd0ec296bcbd552ddda0
Liam Kinne <lkin8911@shared.sydney.edu.au>
2018-01-12 11:05:27 +1100

    added class

--- bop_bit/client.py
+++ bop_bit/client.py
@@ -1,3 +1,24 @@
 # client
 from microbit import *
 import radio
+
+class Client:
+	"""Sends trigger messages to a server on the same channel"""
+	def __init__(self, chan, name):
+		self.channel = chan
+		self.name = name.lower()
+		radio.configure(channel = self.channel)
+		#print("Configured Client")
+
+	def send_trigger():
+		message += self.name
+		message += ":"
+		message += "triggered"
+		radio.send(message)
+		#print("Sent: " + message)
+
+	def send_alive():
+		message += self.name
+		message += ":"
+		message += "alive"
+		#print("Sent: " + message)

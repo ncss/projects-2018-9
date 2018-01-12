b950c432e66cf4c7fe78d337af5003d9676f9c36
giniya5 <gini.artemova@gmail.com>
2018-01-12 17:37:53 +1100

    Updated random number program

--- maze_runner/random_numbers.py
+++ maze_runner/random_numbers.py
@@ -1,6 +1,7 @@
 from microbit import *
 import radio
 import random
+import music
 # RNDM
 
 radio.on()
@@ -8,12 +9,14 @@ radio.config(channel=71)
 
 situp = random.randint(1,8)
 radio.send("RNDM_SNSR-" + str(situp))
+display.show(str(situp))
 print(situp)
 
 while True:
     msg = radio.receive()
     if msg:
         if msg == "SNSR_RNDM-1":
+            music.play(music.ODE, wait=False)
             control = random.randint(1,9)
             radio.send("RNDM_INPT-" + str(control))
             display.show(str(control))

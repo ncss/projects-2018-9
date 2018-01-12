25934ffc638dd6ce37e4ab5622aceae757a99398
IzzyN <isobel.norris@gmail.com>
2018-01-12 15:21:14 +1100

    Added function msg_split

--- SimonSays/unit.py
+++ SimonSays/unit.py
@@ -56,7 +56,26 @@ def button_press(unit_colour):
         continue
     light_all(unit_colour)
 
-#start up
+def round_finished():
+    display.show(Image.HAPPY)
+    #scroll rainbows
+    while != new_game:
+        #scroll rainbowa
+        display.show(Image.HAPPY)
+        
+        msg = radio.receive()
+        if msg:
+            unit_call, instruction, value = msg_split(msg)
+
+def msg_split(msg):
+    msg.split(':')
+    unit_call = msg[0]
+    instruction = msg[1]
+    value = msg[2]
+    return(unit_call, instruction, value)
+        
+
+#START
 radio.on()
 radio.config(channel=73, group=2)
 #to get name from center unit
@@ -66,10 +85,7 @@ npix.clear()
 while set_up == False:
     msg = radio.receive()
     if msg:
-        msg = msg.split(':')
-        unit_call = msg[0]
-        instruction = msg[1]
-        value = msg[2]
+        unit_call, instruction, value = msg_split(msg)
         
         if instruction == 'setup':
             unit_name == unit_call
@@ -80,10 +96,7 @@ while set_up == False:
 while True:
     msg = radio.receive() #EXAMPLE: unit1:colour:red
     if msg:
-        msg = msg.split(':')
-        unit_call = msg[0]
-        instruction = msg[1]
-        value = msg[2]
+        unit_call, instruction, value = msg_split(msg)
         
         if unit_call == unit_name:
             if instruction == 'incorrect':
@@ -94,8 +107,7 @@ while True:
                 unit_colour = value     #sets global value to latest colour
             
     if button_a.was_pressed():          #sends signal to centre
-        button_press(unit_colour)
-        
+        button_press(unit_colour)   
     
 #unit_call:instruction:value
 

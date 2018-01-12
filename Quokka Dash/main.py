bf2eed7a40a98e18d2ba377a50b0f4a08338eb12
Calandra Grima <cgri0612@SHARED.SYDNEY.EDU.AU>
2018-01-12 17:38:55 +1100

    Working version of countdown timer

--- Quokka Dash/main.py
+++ Quokka Dash/main.py
@@ -1,4 +1,12 @@
+from quokka import *
 import drivers.tm1637
 from machine import Pin
 tm = drivers.tm1637.TM1637(clk=Pin('X9'), dio=Pin('X10'))
-tm.numbers(18, 99)
+
+#timer
+TIMER_LENGTH_SECS = 90
+start_time = running_time()
+end_time = start_time + TIMER_LENGTH_SECS*1000
+while running_time() < end_time:
+    remaining_time = (end_time - running_time())//1000
+    tm.numbers(*divmod(remaining_time, 60))

fa19cc507d1c7473748458d3df1d7cd2864e34a4
lily283 <leyang2010@gmail.com>
2018-01-12 15:13:37 +1100

    Changed thresholds

--- bop_bit/shout_it.py
+++ bop_bit/shout_it.py
@@ -3,27 +3,24 @@
 
 from microbit import *
 import radio
-import client
 
 radio.on()
 radio.config(channel=69)
-client = Client(69, "shout_it")
 
 debug = False
 
 def is_triggered():
     prev_val = pin0.read_analog()
-    sleep(200)
+    sleep(100)
     pin_val = pin0.read_analog()
     if debug:
-        print(prev_value, cur_value)
-    if abs(pin_val-prev_val) >= 250:
+        print(prev_val, pin_val)
+    if abs(pin_val-prev_val) >= 550:
         return True
     else:
         return False
     
 while True:
     if is_triggered() == True:
-        client.send_trigger()
         if debug:
             print('yes')

cc22783e614e4355c4720847485763c9d2db89c6
lily283 <leyang2010@gmail.com>
2018-01-13 15:27:49 +1100

    Removed Client class

--- bop_bit/text_to_speech.py
+++ bop_bit/text_to_speech.py
@@ -2,28 +2,8 @@ from microbit import *
 import speech
 import radio
 
-class Client:
-	"""Sends trigger messages to a server on the same channel"""
-	def __init__(self, chan, name):
-		self.channel = chan
-		self.name = name.lower()
-		radio.on()
-		radio.config(channel = self.channel)
-		#print("Configured Client")
-
-	def send_trigger(self):
-		message = self.name
-		message += ":"
-		message += "triggered"
-		radio.send(message)
-		#print("Sent: " + message)
-
-	def send_alive(self):
-		message = self.name
-		message += ":"
-		message += "alive"
-		#print("Sent: " + message)
-
+radio.on()
+radio.config(channel = 69)
 
 sounds = {'bop_it': 'Bop- it?', 'pull_it': 'Pull- it?', 'twist_it': 'Twist- it?', 'shake_it': 'Shake- it?', 
 'spin_it': 'Spin- it?', 'shout_it': 'Shout- it?'}

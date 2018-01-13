f4d3f5769bd2349c2def8406a11fa8a929d08241
Katrina MacDonald <kmac5532@shared.sydney.edu.au>
2018-01-13 20:01:13 +1100

    Switch: changed 'half' to 'mid' attempting to solve radio issue

--- Quokka Dash/pressure_pad.py
+++ Quokka Dash/pressure_pad.py
@@ -5,12 +5,12 @@ radio.config(channel=62)
 
 while True:
     if pin1.read_digital():
-        radio.send('Half')
+        radio.send('mid')
         display.scroll('!Keep Going!')
     elif pin0.read_digital():
-        radio.send('Done')
+        radio.send('done')
         radio.config(channel=72)
-        radio.send('Stop')
+        radio.send('stop')
         display.show(Image.HAPPY)
     else:
         display.show(Image.SAD)

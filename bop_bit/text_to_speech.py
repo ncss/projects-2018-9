e4e6de6e87f9d62904c51ef9138da7495a0f7526
lily283 <leyang2010@gmail.com>
2018-01-14 00:50:37 +1100

    removed sound for pull_it

--- bop_bit/text_to_speech.py
+++ bop_bit/text_to_speech.py
@@ -3,13 +3,14 @@ import speech
 import radio
 import music
 
-radio.on()
-radio.config(channel = 69)
 
-sounds = {'bop_it': 'Bop- it?', 'pull_it': 'Pull- it?', 'twist_it': 'Twist- it?', 'shake_it': 'Shake- it?', 
+sounds = {'bop_it': 'Bop- it?', 'twist_it': 'Twist- it?', 'shake_it': 'Shake- it?', 
 'spin_it': 'Spin- it?', 'shout_it': 'Shout- it?'}
 
 SPEED = 20
+radio.on()
+radio.config(channel=69)
+current_score = 0
 
 while True:
     sound_to_play = radio.receive()
@@ -17,8 +18,20 @@ while True:
         module, is_module = sound_to_play.split(':')
         if is_module == 'is_module' and module in sounds:
             speech.say(sounds[module], speed = 50)
-        elif module == 'score':
-            if is_module == 'lose':
+        elif is_module =='lose':
+            music.play(music.WAWAWAWAA)
+    """
+    score = radio.receive()
+    if score and ':' in score:
+        a, sc = score.split(':')
+        if a == 'score':
+            if sc == 'lose':
                 music.play(music.WAWAWAWAA)
-            else:
-                speech.say(is_module)
+            elif sc in '0123456789' and int(sc) > current_score:
+                music.play(music.BA_DING)
+                current_score = int(sc)
+        elif a == 'speed':
+            music.play(JUMP_DOWN)
+        elif sc == 'is_module' and a in sounds:
+            speech.say(sounds[a], speed = 50)
+    """

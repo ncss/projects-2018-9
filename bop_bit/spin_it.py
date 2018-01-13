1221c82c278021de5c9752bbf53cb1a4d02f6333
AL34180 <AL34180@gmail.com>
2018-01-13 15:29:31 +1100

    Added wait=False to display.show(Image.ALL_CLOCKS)

--- bop_bit/spin_it.py
+++ bop_bit/spin_it.py
@@ -3,7 +3,7 @@
 
 from microbit import *
 import radio
-display.show(Image.ALL_CLOCKS, loop=True, delay=500)
+display.show(Image.ALL_CLOCKS, loop=True, delay=500, wait=False)
 
 class Client:
     """Sends trigger messages to a server on the same channel"""

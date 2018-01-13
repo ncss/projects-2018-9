8d0e6fbe8893512588c1274c08af825a59f48081
lily283 <leyang2010@gmail.com>
2018-01-13 20:55:10 +1100

    fixed invalid syntax

--- bop_bit/text_to_speech.py
+++ bop_bit/text_to_speech.py
@@ -15,4 +15,4 @@ while True:
     if sound_to_play:
         sound_to_play.split(':')
         if sound_to_play[1] == 'is_module' and sound_to_play[0] in sounds:
-            speech.say(sounds[sound_to_play[0]]
+            speech.say(sounds[sound_to_play[0]])

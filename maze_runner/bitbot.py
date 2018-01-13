5f432bf6c5fc3988a71d839f87bf91bdd5cd36d4
Ruchir Bapat <rbap8308@SHARED.SYDNEY.EDU.AU>
2018-01-13 20:18:39 +1100

    Fixed line following

--- maze_runner/bitbot.py
+++ maze_runner/bitbot.py
@@ -1,39 +1,48 @@
 from microbit import *
 import radio
 
-def robot_move(r_value, l_value, slp_time):
-    pin1.write_analog(r_value)
-    pin12.write_digital(0)
-    pin0.write_analog(l_value)
-    pin8.write_digital(0)
-    sleep(slp_time)
-    pin1.write_digital(0)
-    pin12.write_digital(0)
-    pin0.write_digital(0)
-    pin8.write_digital(0)
-    
-def radio_move(direct):
-    if direct == 'forwards':
-        display.show(Image.ARROW_N)
-        robot_move(1023, 1023, 2000)
-        display.clear()
-    elif direct == 'left':
-        display.show(Image.ARROW_W)
-        robot_move(1023, 0, 235)
-        display.clear()
-    elif direct == 'right':
-        display.show(Image.ARROW_E)
-        robot_move(0, 1023, 235)
-        display.clear()
-
 move_commands = ['forwards', 'left', 'right']
+
 radio.on()
 radio.config(channel=71)
-move_for_steps = 2
+move_val = 1023
 
+def robot_move(r_value, l_value):
+    # right motor
+    pin12.write_digital(0)
+    pin1.write_analog(r_value)
+    
+    # left motor
+    pin8.write_digital(0)
+    pin0.write_analog(l_value)
+    
+def robot_stop():
+    pin12.write_digital(0)
+    pin1.write_analog(0)
+    pin8.write_digital(0)
+    pin0.write_analog(0)
+
+def radio_move(direct):
+    if direct == 'forwards':
+        robot_move((move_val >> 1) + 150, (move_val >> 1))
+    elif direct == 'left':
+        #robot_move(move_val, 0, 235)
+        robot_move(move_val, 0)
+    elif direct == 'right':
+        #robot_move(0, move_val, 235)
+        robot_move(0, move_val)
+    
+    MOVE_TIME = 1000
+    stop_time = running_time() + MOVE_TIME
+    
+    while (running_time() <= stop_time) and (pin11.read_digital() or pin5.read_digital()):
+        continue
+    robot_stop()
+    
 while True:
     radio_msg = radio.receive()
     if radio_msg and radio_msg.startswith("INPT_BTBT-"):
         msg_command = radio_msg.split('-')[1]
-        if msg_command in move_commands:
-            radio_move(msg_command)
+        #if msg_command in move_commands:
+        #    radio_move(msg_command)
+        radio_move(radio_msg[10:])

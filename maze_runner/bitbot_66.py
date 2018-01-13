4a4d8c3bbad5c31d0b085802096c6b0a1e39cf67
Ruchir Bapat <rbap8308@SHARED.SYDNEY.EDU.AU>
2018-01-13 22:00:16 +1100

    using channel 66 as well now

---
+++ maze_runner/bitbot_66.py
@@ -0,0 +1,53 @@
+from microbit import *
+import radio
+
+move_commands = ['forwards', 'left', 'right']
+
+radio.on()
+radio.config(channel=66)
+move_val = 1023
+
+def robot_move(r_value, l_value):
+    # right motor
+    pin12.write_digital(0)
+    pin1.write_analog(r_value)
+    
+    # left motor
+    pin8.write_digital(0)
+    pin0.write_analog(l_value)
+    
+def robot_stop():
+    pin12.write_digital(0)
+    pin1.write_analog(0)
+    pin8.write_digital(0)
+    pin0.write_analog(0)
+
+def radio_move(direct):
+    display.show(direct[0])
+    move_time = 0
+    if direct == 'forwards':
+        robot_move((move_val >> 1) + 150, (move_val >> 1))
+        move_time = 1000
+    elif direct == 'left':
+        #robot_move(move_val, 0, 235)
+        robot_move(move_val, 0)
+        move_time = 235
+    elif direct == 'right':
+        #robot_move(0, move_val, 235)
+        robot_move(0, move_val)
+        move_time = 235
+    
+    stop_time = running_time() + move_time
+    
+    while (running_time() <= stop_time) and (pin11.read_digital() or pin5.read_digital()):
+        continue
+    robot_stop()
+    
+while True:
+    radio_msg = radio.receive()
+    if radio_msg and radio_msg.startswith("INPT_BTBT-"):
+        msg_command = radio_msg.split('-')[-1]
+        if msg_command in move_commands:
+            radio_move(msg_command)
+        #radio_move(radio_msg[10:])
+    radio.on()
---
+++ maze_runner/sensor_66.py
@@ -0,0 +1,37 @@
+from microbit import *
+import radio
+
+situps_left = 0
+prev = accelerometer.current_gesture()
+curr = prev
+radio.on()
+radio.config(channel=66)
+while True:
+    if situps_left:
+        display.show(str(situps_left))
+        temp = accelerometer.current_gesture()
+        
+        if temp:
+            curr = temp
+        
+        if curr == "up" and prev == "face up":
+            #print("sitting up")
+            situps_left -= 1
+        
+        display.show(str(situps_left))
+        
+        if not situps_left:
+            for in range(3):
+                radio.send("SNSR_RNDM-1")
+                sleep(50)
+            display.show(Image.HAPPY)
+            radio.on()
+        
+        prev = curr
+        radio.on()
+        
+    else:
+        msg = radio.receive()
+        if msg and msg.startswith("RNDM_SNSR-"):
+            situps_left = int(msg[10])
+            print(str(situps_left))

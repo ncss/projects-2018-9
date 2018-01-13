f9740997d4958b2618ab225bec69c54c0886d1ec
Liam Kinne <lkin8911@SHARED.SYDNEY.EDU.AU>
2018-01-13 12:51:37 +1100

    fixed merge

--- bop_bit/bop_it.py
+++ bop_bit/bop_it.py
@@ -9,7 +9,7 @@ class Server:
     """docstring for Server"""
     def __init__(self, chan):
         self.channel = chan
-        self.modules = {}
+        self.modules = {"bop_it": False, "twist_it": False}
         radio.on()
         radio.config(channel = self.channel)
 
@@ -23,72 +23,59 @@ class Server:
         if module in self.modules:
             state = self.modules[module]
             self.modules[module] = False
+            #print(self.modules)
         else:
             state = False
         return state
 
     def set_module_was_triggered(self, module):
-        if module in self.modules:
-            modules[module] = True
+        self.modules[module] = True
 
 server = Server(69)
 
 debug = True
 
-def is_triggered():
-    pressed = pin0.read_digital()
-    if pressed == 0:
-        return True
+pin0.set_pull(pin0.PULL_UP)
+pressed_last_state = False
+def is_triggered(prev_state):
+    state = pin0.read_digital()
+    if state and not prev_state:
+        return True, state
     else:
-        return False
+        return False, state
 
 """ GAME STUFF """
 
-def set_to_default_game_state():
-    if debug: print("set_to_default_game_state")
-    timeout = 5000
-    module_current = ""
-    lives = 3
-    begin_time = running_time()
-    score = 0
-    speed = 0
-
-def pick_next_module():
-    if debug: print("pick_next_module")
-    module_current = random.choice(server.modules)
-    begin_time = running_time()
-    # say module with text to speech
+score = 0
+lives = 3
+timeout = 5000
+speed = 0
+trigger_last = False
 
 while True:
-    while not is_triggered():
-        # start game!
-        set_to_default_game_state()
-        pick_next_module()
-        while True:
-            # check timeout
-            if (running_time() - begin_time) >= (timeout - speed):
-                # ran out of time!
-                if (lives <= 0):
-                    # die
-                    # play bad sound
-                    # incomplete
-                    # send score to leaderboard
-                    sleep(5000)
-                    break # go back to waiting for button press
-                else:
-                    lives -= 1 # remove a life
-                continue
-
-            if is_triggered():
+    module_current = random.choice(list(server.modules.keys()))
+    print(module_current)
+    begin_time = running_time()
+    while True:
+        if (running_time() - begin_time) >= (timeout - speed):
+            if lives <= 0:
+                print("die!")
+                while True:
+                    pass
+            else:
+                print("lose!")
+                lives -= 1
+                break
+        else:
+            trigger = pin0.read_digital()
+            if trigger and not trigger_last:
+                print("bopped")
                 server.set_module_was_triggered("bop_it")
+            trigger_last = trigger
             
             if server.get_module_was_triggered(module_current):
-                if debug: print("module_was_triggered")
-                # yay! go to next
-                score += 1 # incriment score
-                # increase speed
-                if speed > 1000:
-                    speed += 10
-                # pick next module
-                pick_next_module()
-                continue
+                print("win!")
+                score += 1
+                speed += 1
+                break
+
